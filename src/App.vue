64e3757e68adf8981273348de6a8a2edbf91b31a
oluwafemi medalem <oluwafemi.medale@andela.com>
2018-03-06 09:43:05 +0100

    initial commit :fire:

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <active></active>
+  </div>
+</template>
+
+<script>
+import Hello from './components/HelloWorld'
+import Active from './components/Active'
+export default {
+  name: 'App',
+  components: {
+    Hello,
+    Active
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
